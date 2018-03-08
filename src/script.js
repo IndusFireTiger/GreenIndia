9c4e36267674d6739eb17701c0ee77ad8ba94d85
indusfiretiger <indusfiretiger@gmail.com>
2018-03-08 19:17:53 +0530

    Listed States on left section

--- src/script.js
+++ src/script.js
@@ -1,14 +1,28 @@
-var svg = d3.select("svg"),
+let states = document.getElementById("states-list")
+d3.json("States_wise_data.json", function(error, stateData){
+    if (error) {
+        throw error;
+    }
+    for(let i = 0; i < stateData.data.length; i++){
+        console.log("states:"+stateData.data[i][0])
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(stateData.data[i][0]));
+        states.appendChild(li)
+    }
+    
+})
+
+let svg = d3.select("svg"),
     margin = 200,
     width = svg.attr("width") - margin,
     height = svg.attr("height") - margin;
 
 console.log(d3)
-var xScale = d3.scaleBand().range([0, width]).padding(0.5),
+let xScale = d3.scaleBand().range([0, width]).padding(0.5),
     yScale = d3.scaleLinear().range([height, 0]);
 console.log(xScale)
-var g = svg.append("g")
-    .attr("transform", "translate(" + 100 + "," + 100 + ")");
+let g = svg.append("g")
+    .attr("transform", "translate(100, 100)");
 
 console.log(g)
 
@@ -17,9 +31,10 @@ d3.csv("./Total Tree Cover in India.csv", function (error, data) {
         throw error;
     }
     console.log("data:" + data)
-
+    console.log("max:"+(0.1+d3.max(data, function (d) { return d.share_TreeCover; })))
     xScale.domain(data.map(function (d) { return d.year; }));
-    yScale.domain([0, d3.max(data, function (d) { return d.share_TreeCover; })]);
+    // yScale.domain([0, 1+3.04]);
+    yScale.domain([0, 1 + parseInt(d3.max(data, function (d) { return d.share_TreeCover; }))]);
 
     // console.log(d3.axisBottom(xScale)) // returns a function which is then called with child g as a parameter
     g.append("g")
@@ -27,7 +42,7 @@ d3.csv("./Total Tree Cover in India.csv", function (error, data) {
         .call(d3.axisBottom(xScale));
 
     g.append("g")
-        .call(d3.axisLeft(yScale).ticks(10))
+        .call(d3.axisLeft(yScale).ticks(5))
         .append("text")
         .attr("y", 6)
         .attr("dy", "0.71em")
