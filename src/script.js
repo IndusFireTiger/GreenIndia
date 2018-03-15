58f30a569d1379c784c2c6cbb314d2e57296675a
indusfiretiger@gmail.com <indusfiretiger@gmail.com>
2018-03-15 13:08:27 +0530

    rearrange before refactor

--- src/script.js
+++ src/script.js
@@ -1,3 +1,169 @@
+function plotIndiaMap() {
+    var w = 600
+    var h = 600
+    var proj = d3.geo.mercator()
+    var path = d3.geo.path().projection(proj)
+    var t = proj.translate()
+    var s = proj.scale()
+
+    var map = d3.select("#chart").append("svg:svg")
+        .attr("width", w)
+        .attr("height", h)
+        .call(initialize)
+
+    var india = map.append("svg:g")
+        .attr("id", "india")
+    var svg = d3.select("svg")
+
+    var tooltip = d3.select("#chart").append("div").attr("class", "tooltip hidden")
+    var offsetL = document.getElementById('chart').offsetLeft + 10
+    var offsetT = document.getElementById('chart').offsetTop + -30
+
+    d3.json("states.json", function (json) {
+        states = json.features
+        india.selectAll("path")
+            .data(states)
+            .enter().append("path")
+            .attr("d", path)
+            .attr("title", function (d, i) {
+                return d.id
+            })
+            .on("mousemove", function (d, i) {
+                d3.select(this).attr('class', 'highlight_state')
+                var mouse = d3.mouse(svg.node()).map(function (d) {
+                    return parseInt(d)
+                })
+                tooltip.classed("hidden", false)
+                    .attr("style", "left:" + (mouse[0] + offsetL) + "px;top:" + (mouse[1] + offsetT) + "px")
+                    .html(d.id)
+            })
+            .on("mouseout", function (d, i) {
+                d3.select(this).attr('class', '')
+                tooltip.classed("hidden", true)
+            })
+            .on("click", function (d, i) {
+                console.log(d.id + " was clicked")
+                // invoke func to plot state graph
+                plotStateGraph(d.id)
+            })
+    })
+
+    function initialize() {
+        proj.scale(6700)
+        proj.translate([-1240, 720])
+    }
+
+}
+
+function plotOverviewGraph() {
+    let svg = d3.select("svg"),
+        margin = 200,
+        width = svg.attr("width") - margin,
+        height = svg.attr("height") - margin
+    let xScale = d3.scaleBand().padding(0.5),
+        yScale = d3.scaleLinear()
+    let g = svg.append("g")
+        .attr("transform", "translate(100, 100)")
+
+    d3.json("Total Tree Cover in India.json", function (error, data) {
+        if (error) {
+            throw error
+        }
+
+        xScale.range([0, width]).domain(data.data.map(function (d) {
+            return d[0]
+        }))
+
+        yScale.range([height, 0]).domain([0, 1 + parseInt(d3.max(data.data, function (d) {
+            return d[3]
+        }))])
+
+        let xAxis_g = g.append("g")
+            .attr("transform", "translate(0," + height + ")")
+        xAxis_g.call(d3.axisBottom(xScale))
+        xAxis_g.append("text")
+            .attr("y", height - 250)
+            .attr("x", width - 100)
+            .attr("dx", "5em")
+            .attr("text-anchor", "end")
+            .attr("stroke", "black")
+            .style("font-size","16px")
+            .text("Year")
+
+        let yAxis_g = g.append("g")
+        yAxis_g.call(d3.axisLeft(yScale).ticks(5))
+        yAxis_g.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", "-4em")
+            .attr("text-anchor", "end")
+            .attr("stroke", "black")
+            .style("font-size","16px")
+            .text("Forest Cover in %")
+
+        let bars = g.selectAll(".bar")
+
+        bars.data(data.data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .on("mouseover", onMouseOver)
+            .on("mouseout", onMouseOut)
+            .attr("x", function (d) {
+                return xScale(d[0])
+            })
+            .attr("y", function (d) {
+                return yScale(d[3])
+            })
+            .attr("width", xScale.bandwidth())
+            .attr("height", function (d) {
+                return height - yScale(d[3])
+            })
+    })
+
+    function onMouseOver(d, i) {
+        d3.select(this).attr('class', 'highlight_bar')
+        d3.select(this)
+            .transition()
+            .duration(400)
+            .attr('width', xScale.bandwidth() + 5)
+            .attr("y", function (d) {
+                return yScale(d[3]) - 10
+            })
+            .attr("height", function (d) {
+                return height - yScale(d[3]) + 10
+            })
+
+        g.append("text")
+            .attr('class', 'val')
+            .attr('x', function () {
+                return xScale(d[0])
+            })
+            .attr('y', function () {
+                return yScale(d[3]) - 15
+            })
+            .text(function () {
+                return [d[3] + " %"]
+            })
+    }
+
+    function onMouseOut(d, i) {
+        d3.select(this).attr('class', 'bar')
+        d3.select(this)
+            .transition()
+            .duration(400)
+            .attr('width', xScale.bandwidth())
+            .attr("y", function (d) {
+                return yScale(d[3])
+            })
+            .attr("height", function (d) {
+                return height - yScale(d[3])
+            })
+
+        d3.selectAll('.val')
+            .remove()
+    }
+}
+
 function clearGraph() {
     let svg = d3.selectAll('svg')
     let gs = svg.selectAll('g').selectAll('g')
@@ -143,178 +309,11 @@ let plotStateGraph = function (state) {
     })
 }
 
-function plotOverviewGraph() {
-    let svg = d3.select("svg"),
-        margin = 200,
-        width = svg.attr("width") - margin,
-        height = svg.attr("height") - margin
-    let xScale = d3.scaleBand().padding(0.5),
-        yScale = d3.scaleLinear()
-    let g = svg.append("g")
-        .attr("transform", "translate(100, 100)")
-
-    d3.json("Total Tree Cover in India.json", function (error, data) {
-        if (error) {
-            throw error
-        }
-
-        xScale.range([0, width]).domain(data.data.map(function (d) {
-            return d[0]
-        }))
-
-        yScale.range([height, 0]).domain([0, 1 + parseInt(d3.max(data.data, function (d) {
-            return d[3]
-        }))])
-
-        let xAxis_g = g.append("g")
-            .attr("transform", "translate(0," + height + ")")
-        xAxis_g.call(d3.axisBottom(xScale))
-        xAxis_g.append("text")
-            .attr("y", height - 250)
-            .attr("x", width - 100)
-            .attr("dx", "5em")
-            .attr("text-anchor", "end")
-            .attr("stroke", "black")
-            .style("font-size","16px")
-            .text("Year")
-
-        let yAxis_g = g.append("g")
-        yAxis_g.call(d3.axisLeft(yScale).ticks(5))
-        yAxis_g.append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", "-4em")
-            .attr("text-anchor", "end")
-            .attr("stroke", "black")
-            .style("font-size","16px")
-            .text("Forest Cover in %")
-
-        let bars = g.selectAll(".bar")
-
-        bars.data(data.data)
-            .enter().append("rect")
-            .attr("class", "bar")
-            .on("mouseover", onMouseOver)
-            .on("mouseout", onMouseOut)
-            .attr("x", function (d) {
-                return xScale(d[0])
-            })
-            .attr("y", function (d) {
-                return yScale(d[3])
-            })
-            .attr("width", xScale.bandwidth())
-            .attr("height", function (d) {
-                return height - yScale(d[3])
-            })
-    })
-
-    function onMouseOver(d, i) {
-        d3.select(this).attr('class', 'highlight_bar')
-        d3.select(this)
-            .transition()
-            .duration(400)
-            .attr('width', xScale.bandwidth() + 5)
-            .attr("y", function (d) {
-                return yScale(d[3]) - 10
-            })
-            .attr("height", function (d) {
-                return height - yScale(d[3]) + 10
-            })
-
-        g.append("text")
-            .attr('class', 'val')
-            .attr('x', function () {
-                return xScale(d[0])
-            })
-            .attr('y', function () {
-                return yScale(d[3]) - 15
-            })
-            .text(function () {
-                return [d[3] + " %"]
-            })
-    }
-
-    function onMouseOut(d, i) {
-        d3.select(this).attr('class', 'bar')
-        d3.select(this)
-            .transition()
-            .duration(400)
-            .attr('width', xScale.bandwidth())
-            .attr("y", function (d) {
-                return yScale(d[3])
-            })
-            .attr("height", function (d) {
-                return height - yScale(d[3])
-            })
-
-        d3.selectAll('.val')
-            .remove()
-    }
-}
-
-function plotIndiaMap() {
-    var w = 600
-    var h = 600
-    var proj = d3.geo.mercator()
-    var path = d3.geo.path().projection(proj)
-    var t = proj.translate()
-    var s = proj.scale()
-
-    var map = d3.select("#chart").append("svg:svg")
-        .attr("width", w)
-        .attr("height", h)
-        .call(initialize)
-
-    var india = map.append("svg:g")
-        .attr("id", "india")
-    var svg = d3.select("svg")
-
-    var tooltip = d3.select("#chart").append("div").attr("class", "tooltip hidden")
-    var offsetL = document.getElementById('chart').offsetLeft + 10
-    var offsetT = document.getElementById('chart').offsetTop + -30
-
-    d3.json("states.json", function (json) {
-        states = json.features
-        india.selectAll("path")
-            .data(states)
-            .enter().append("path")
-            .attr("d", path)
-            .attr("title", function (d, i) {
-                return d.id
-            })
-            .on("mousemove", function (d, i) {
-                d3.select(this).attr('class', 'highlight_state')
-                var mouse = d3.mouse(svg.node()).map(function (d) {
-                    return parseInt(d)
-                })
-                tooltip.classed("hidden", false)
-                    .attr("style", "left:" + (mouse[0] + offsetL) + "px;top:" + (mouse[1] + offsetT) + "px")
-                    .html(d.id)
-            })
-            .on("mouseout", function (d, i) {
-                d3.select(this).attr('class', '')
-                tooltip.classed("hidden", true)
-            })
-            .on("click", function (d, i) {
-                console.log(d.id + " was clicked")
-                // invoke func to plot state graph
-                plotStateGraph(d.id)
-            })
-    })
-
-    function initialize() {
-        proj.scale(6700)
-        proj.translate([-1240, 720])
-    }
-
-}
-
 function populateOverview() {
     // populateStates()
     plotOverviewGraph()
     plotIndiaMap()
 }
-
 
 populateOverview()
 
