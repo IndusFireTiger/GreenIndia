9171742fdb6623776df85d07ce732eb65248ef84
indusfiretiger@gmail.com <indusfiretiger@gmail.com>
2018-03-15 08:55:41 +0530

    server serving different content

--- src/server.js
+++ src/server.js
@@ -1,23 +1,39 @@
 'use strict'
 let http = require('http')
-let server = http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'})
+let fs = require('fs')
+
+let server = http.createServer(function (req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    })
     console.log('request was made : ' + req.url)
-    // res.writeHead(200, {'Content-Type': 'application/json'})
-    // res.write('hello world')
-    let obj = {
-        name: "sindhu",
-        school: "geek"
+    if (req.url == '/home' || req.url === '/') {
+        console.log('dirname' + __dirname)
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        fs.createReadStream(__dirname + '/index.html').pipe(res)
     }
-    // res.write(req.url)
-    // res.end()
-    res.end(JSON.stringify(obj))
+    if (req.url == '/ForestReport') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        fs.createReadStream(__dirname + '/ForestInIndia.html').pipe(res)
+    }
+    if (req.url == '/StateData') {
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        })
+        let data = fs.createReadStream(__dirname + '/States_wise_data.json').pipe(res)
+        // console.log(data)
+        // res.end(JSON.stringify(data))
+    }
 })
+
 let port = 5432
-// server.listen(port)
 server.listen(port, (err) => {
     if (err) {
-      throw Error('something bad happened', err)
-    }  
-    console.log(`server is listening on ${port}`)
-  })
+        return console.log('something bad happened', err)
+    }
+    console.log('server is listening on '+port)
+})
